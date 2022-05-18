dff42383a0bea6e7858b6c12d25c438d33eedddc
vovakhrom <71398339+vovakhrom@users.noreply.github.com>
2022-05-18 11:28:47 +0300

    Add files via upload

---
+++ java/com/cometchat/pro/androiduikit/constants/AppConfig.java
@@ -0,0 +1,14 @@
+package com.cometchat.pro.androiduikit.constants;
+
+public class AppConfig {
+
+    public class AppDetails {
+
+        public static final String APP_ID = "2099928cc39a4c86";
+
+        public static final String AUTH_KEY = "524adfeeefb2670337a2159d09033f817e863992";
+
+        public static final String REGION = "eu";
+    }
+}
+
